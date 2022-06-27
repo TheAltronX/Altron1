b4d9a437f7f57ba869c787fc389e08e1605796b4
MK <101259067+ERR0rMK@users.noreply.github.com>
2022-06-27 21:28:09 +0530

    Add files via upload

---
+++ Altron/modules/helper_funcs/telethn/__init__.py
@@ -0,0 +1,7 @@
+from Altron import DEV_USERS, PYTHON, DEMONS, TIGERS, WOLVES, telethn
+
+AltronUsers = PYTHON + WOLVES + DEMONS + TIGERS + DEV_USERS
+
+AltronUsers = (
+    list(PYTHON) + list(WOLVES) + list(DEMONS) + list(TIGERS) + list(DEV_USERS)
+)
